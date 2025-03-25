f61d75ec292aa1fe9c9f146b5d56ed96a7e0760f
Dimitris Stavroulakis <jimstav@gmail.com>
2025-03-25 12:54:26 +0200

    Implementing an Analyzer Class

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
@@ -10,14 +11,5 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games.`);
+const winsAnalysis = new WinsAnalysis('Man United');
+console.log(winsAnalysis.run(matchReader.matches));
